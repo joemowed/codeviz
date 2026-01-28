c401c3eef59051d848eb150cd09bb05227e4ad43
Joe Maloney <maloney.jg2@gmail.com>
2026-01-28 14:38:05 -0500

    Add feature to auto detect VSCC output directory

--- src/extension.ts
+++ src/extension.ts
@@ -6,58 +6,64 @@ import { VSCCDataPrep } from "./data/vscc_dataprep";
 import { Summary } from "./data/vscc_result";
 
 export function activate(context: ExtensionContext) {
-    const importCommand = commands.registerCommand("CodeViz.import", async () => {
-        let wsPath: Uri;
-        workspace.workspaceFolders?.map((folder) => {
-            if (wsPath == undefined)
-                wsPath = folder.uri;
-        });
-        const folderPath = await window.showInputBox({ 
-            placeHolder: "import path",
-            prompt: "Enter import data path",
-            value: ""
-        });
-        if (folderPath) {
+  const importCommand = commands.registerCommand("CodeViz.import", async () => {
+    let wsPath: Uri;
+    workspace.workspaceFolders?.map((folder) => {
+      if (wsPath == undefined) wsPath = folder.uri;
+    });
+    const folderPath = await window.showInputBox({
+      placeHolder: "import path",
+      prompt: "Enter import data path",
+      value: "",
+    });
+    if (folderPath) {
+      CCVizzuPanel.render(context.extensionUri).then(() => {
+        CCVizzuPanel.import(folderPath);
+      });
+    }
+  });
+  const exportCommand = commands.registerCommand("CodeViz.export", async () => {
+    const folderPath = await window.showInputBox({
+      placeHolder: "export path",
+      prompt: "Enter export data path",
+      value: "",
+    });
+    if (folderPath) {
+      CCVizzuPanel.export(folderPath);
+    }
+  });
+  const showCommand = commands.registerCommand(
+    "CodeViz.show",
+    (targetDir: Uri | undefined) => {
+      let wsPath: Uri;
+      workspace.workspaceFolders?.map((folder) => {
+        if (wsPath == undefined) wsPath = folder.uri;
+      });
+      commands
+        .executeCommand("extension.vscode-counter.countInDirectory", targetDir)
+        .then(() => {
+          setTimeout(() => {
             CCVizzuPanel.render(context.extensionUri).then(() => {
-                CCVizzuPanel.import(folderPath);
+              let source: VSCCDataSource;
+              if (targetDir == undefined) targetDir = wsPath;
+              source = new VSCCDataSource(wsPath);
+              if (source.data != undefined) {
+                let data = new VSCCDataPrep();
+                data.makeDataTable(source.data);
+                CCVizzuPanel.refresh(
+                  data.getDataTable(),
+                  new Summary(source, data),
+                );
+              } else
+                window.showErrorMessage(
+                  "'Code Counter' data is not awailable!",
+                );
             });
-        }
-    });
-    const exportCommand = commands.registerCommand("CodeViz.export", async () => {
-        const folderPath = await window.showInputBox({ 
-            placeHolder: "export path",
-            prompt: "Enter export data path",
-            value: ""
+          }, 1000);
         });
-        if (folderPath) {
-            CCVizzuPanel.export(folderPath);
-        }
-    });
-    const showCommand = commands.registerCommand("CodeViz.show", (targetDir: Uri | undefined) => {
-        let wsPath: Uri;
-        workspace.workspaceFolders?.map((folder) => {
-            if (wsPath == undefined)
-                wsPath = folder.uri;
-        });
-        commands.executeCommand('extension.vscode-counter.countInDirectory', targetDir).then(() => {
-            setTimeout(() => {
-                CCVizzuPanel.render(context.extensionUri).then(() => {
-                    let source: VSCCDataSource;
-                    if (targetDir == undefined)
-                        targetDir = wsPath;
-                    source = new VSCCDataSource(wsPath, targetDir);
-                    if (source.data != undefined) {
-                        let data = new VSCCDataPrep();
-                        data.makeDataTable(source.data);
-                        CCVizzuPanel.refresh(data.getDataTable(), new Summary(source, data));
-                    }
-                    else
-                        window.showErrorMessage('\'Code Counter\' data is not awailable!');
-                });                    
-            }, 1000);
-        });
-    });
-    context.subscriptions.push(showCommand);
-    context.subscriptions.push(importCommand);
-    context.subscriptions.push(exportCommand);
+    },
+  );
+  context.subscriptions.push(showCommand);
+  context.subscriptions.push(importCommand);
+  context.subscriptions.push(exportCommand);
 }
